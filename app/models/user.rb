f65787ebc5d31145545f6c8a4784de607ad61428
anikonchuk <anikonch@gmail.com>
2018-08-27 15:31:16 -0400

    Add slug and find_by_slug to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   has_many :writers, through: :comics
   has_many :artists, through: :comics
 
+  def slug
+    self.username.downcase.gsub(/ /, "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |username|
+      username.slug == slug
+    end
+  end
+
 end
