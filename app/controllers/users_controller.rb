0a58310a43950297d31bd65bdd90e8330b1f0113
anikonchuk <anikonch@gmail.com>
2018-08-28 13:24:52 -0400

    Start User show page view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   get '/users/:slug' do
     if logged_in?
       @user = User.find_by_slug(params[:slug])
-      erb '/users/show'
+      erb :'/users/show'
     else
       redirect '/'
     end
