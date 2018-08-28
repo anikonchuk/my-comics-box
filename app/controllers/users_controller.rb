a559eafbee7d295610a2ec211864e42d5cb83f8b
anikonchuk <anikonch@gmail.com>
2018-08-28 13:13:57 -0400

    Create post '/login' route in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,18 @@ class UsersController < ApplicationController
       erb :'/users/sign_in'
     else
       redirect "/users/#{current_user.slug}"
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.slug}"
+    else
+      flash[:message] = "Log In failed. Please try again"
+      redirect '/login'
     end 
   end
 
