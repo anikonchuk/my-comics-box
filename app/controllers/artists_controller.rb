77061d228ce266e49c3deaad23d16190dc87aac2
anikonchuk <anikonch@gmail.com>
2018-08-28 19:39:39 -0400

    Add Index route to ArtistsController

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,12 @@
 class ArtistsController < ApplicationController
 
+  get '/artists' do
+    if logged_in?
+      @artists = Artist.all
+      erb :'/artists/index'
+    else
+      redirect '/'
+    end
+  end
+
 end
