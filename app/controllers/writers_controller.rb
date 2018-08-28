4f331106d16f52bb0bd34989a05476ea1985cf43
anikonchuk <anikonch@gmail.com>
2018-08-28 19:35:14 -0400

    Add Index route to WritersController

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -1,3 +1,12 @@
 class WritersController < ApplicationController
 
+  get '/writers' do
+    if logged_in?
+      @writers = Writer.all
+      erb :'/writers/index'
+    else
+      redirect '/'
+    end
+  end
+
 end
