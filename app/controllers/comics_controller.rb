262ea44020414722bc30ac70d03a74205cdc067c
anikonchuk <anikonch@gmail.com>
2018-08-28 15:40:54 -0400

    Add index route to ComicsController

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,3 +1,12 @@
 class ComicsController < ApplicationController
 
+  get '/comics' do
+    if logged_in?
+      @comics = Comic.all
+      erb :'/comics/index'
+    else
+      redirect '/'
+    end
+  end
+
 end
